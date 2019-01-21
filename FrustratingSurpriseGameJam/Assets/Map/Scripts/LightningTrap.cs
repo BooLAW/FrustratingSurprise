5bf96728639d7db46c6608f440df8a3046532e90
rodrigodpl <rodrigodpl97@gmail.com>
2019-01-21 13:50:15 +0100

    fixes

---
+++ FrustratingSurpriseGameJam/Assets/Map/Scripts/LightningTrap.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningTrap : TrapBaseClass
+{
+    private BoxCollider2D death_collider;
+    private SpriteRenderer spriteRenderer;
+
+    public void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+        BoxCollider2D[] colliders = GetComponents<BoxCollider2D>();
+
+        foreach (BoxCollider2D col in colliders)
+        {
+            if (!col.isTrigger)
+            {
+                death_collider = col;
+                return;
+            }
+        }      
+    }
+
+    override public void childUpdate()
+    {
+        if (state == TrapState.ACTIVATING)
+        {
+            death_collider.enabled = true;
+            spriteRenderer.enabled = true;
+            state = TrapState.ACTIVE;
+            reset_timer = Time.time;
+
+        }
+        else if (state == TrapState.DEACTIVATING)
+        {
+            death_collider.enabled = false;
+            spriteRenderer.enabled = false;
+            state = TrapState.INACTIVE;
+            reset_timer = Time.time;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player" && state == TrapState.INACTIVE)
+        {
+            state = TrapState.ACTIVATING;
+        }
+    }
+
+    override public void childReset()
+    {
+        death_collider.enabled = false;
+        spriteRenderer.enabled = false;
+        state = TrapState.INACTIVE;
+    }
+
+}
