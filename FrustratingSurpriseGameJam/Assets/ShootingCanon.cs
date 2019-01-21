d8dfe9398370497196fd2e10e3d5c8352866476f
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2019-01-21 13:40:13 +0100

    Added Canon

---
+++ FrustratingSurpriseGameJam/Assets/ShootingCanon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingCanon : MonoBehaviour {
+
+    private float timeBtWShots;
+    public float startTimeBtWShots;
+    public GameObject projectile;
+    private Transform player;
+    bool first_instance = false;
+    // Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        first_instance = (name == "Projectile");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (first_instance)
+            return;
+		if(timeBtWShots <=0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtWShots = startTimeBtWShots;
+        }
+        else
+        {
+            timeBtWShots -= Time.deltaTime;
+        }
+	}
+}
