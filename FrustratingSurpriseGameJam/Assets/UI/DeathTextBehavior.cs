ed5bf49c83dfe1f1674fc8ada5aa84e598166950
rodrigodpl <rodrigodpl97@gmail.com>
2019-01-21 19:14:30 +0100

    In-game UI

---
+++ FrustratingSurpriseGameJam/Assets/UI/DeathTextBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathTextBehavior : MonoBehaviour {
+
+    private PlayerController player;
+    private Text text;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        string new_text = "Deaths: " + player.death_count.ToString("00");
+        if (new_text != text.text)
+            text.text = new_text;
+    }
+}
---
+++ FrustratingSurpriseGameJam/Assets/UI/TimerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerBehavior : MonoBehaviour {
+
+    private float start_time = 0.0f;
+    private Text text;
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        start_time = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float current_time = Time.time - start_time;
+        string new_text = ((int)(current_time / 60.0f)).ToString("00") + " : " + ((int)(current_time % 60)).ToString("00");
+        if (new_text != text.text)
+            text.text = new_text;
+	}
+
+    public void Restart()
+    {
+        start_time = Time.time;
+    }
+}
