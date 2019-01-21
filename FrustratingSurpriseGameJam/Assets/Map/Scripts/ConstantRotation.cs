4c6c3dd4b1c80be3aa117991914cc4d278cc11cd
rodrigodpl <rodrigodpl97@gmail.com>
2019-01-21 16:52:59 +0100

    Rotating platform

---
+++ FrustratingSurpriseGameJam/Assets/Map/Scripts/ConstantRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour {
+
+    public float speed = 2.0f;
+
+	void Update () {
+        transform.parent.Rotate(new Vector3(0.0f, 0.0f, speed));
+	}
+}
