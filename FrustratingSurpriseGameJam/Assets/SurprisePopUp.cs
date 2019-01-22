7f83b5b0abc9c3f0f48969e7d9baf5749536a9a2
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2019-01-22 11:07:09 +0100

    Added Sounds + WiP Surprise Text

---
+++ FrustratingSurpriseGameJam/Assets/SurprisePopUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurprisePopUp : MonoBehaviour {
+
+    public GameObject player;
+    public int death_count;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
